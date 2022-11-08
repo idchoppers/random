ff4d215027b3ffef868b65a92f3088ad3c128175
idchoppers <anonymous344@protonmail.com>
2022-11-07 21:53:31 -0500

    On branch master  Changes to be committed: 	modified:   Java/Finance/Formulas.java
    remove debug comment

--- financeExercises/Java/Finance/Formulas.java
+++ financeExercises/Java/Finance/Formulas.java
@@ -107,32 +107,4 @@ public class Formulas {
     public static double lifetimeWealthRatio(double netWorth, double totalLifetimeIncome) {
 	return netWorth / totalLifetimeIncome;
     }
-
-    /*
-    public static void main(String[] args) {
-	System.out.format("%.2f\n", loanAmort(20000.00, 0.00625, 60));
-	System.out.format("%.2f\n", totalAssets(100000.00, 50000.00));
-	System.out.format("%.2f\n", netWorth(100.00, 50.00));
-	System.out.format("%.2f\n", breakEvenPrice(100000.00, 200.00, 0.4));
-	System.out.format("%.2f\n", breakEvenUnits(100000.00, 500.4, 0.4));
-	System.out.format("%.2f\n", simpleInterest(100.00, 0.07, 60));
-	System.out.format("%.2f\n", compoundInterest(100.00, 0.07, 0.09, 60));
-	System.out.format("%.2f\n", profitMargin(2000.00, 500.00));
-	System.out.format("%.2f\n", netIncome(500.00, 2000.00));
-	System.out.format("%.2f\n", burnRatePercentage(100000.00, 2000.00));
-	System.out.println("START CHART");
-	for(int i = 0; i < 4; i++) {
-	    double[] chart = fiftyThirtyTwenty(4000.00);
-	    System.out.format("%.2f\n", chart[i]);
-	}
-	System.out.println("END CHART");
-	System.out.format("%.2f\n", ruleOf72(0.07));
-	System.out.format("%.2f\n", ruleOf25(20000.00));
-	System.out.format("%.2f\n", fourPercentWithdrawl(100000.00));
-	System.out.format("%.2f\n", averageNetIncome(200000.00, 25000.00));
-	System.out.format("%.2f\n", hoursOfLifeAsMoneyFormal(200000.00, 25000.00, 260, 8));
-	System.out.format("%.2f\n", hoursOfLifeAsMoneyReal(200000.00, 25000.00, 3500.00, 260, 8, 400));
-	System.out.format("%.2f\n", purchaseAsHoursOfLifeEquivilent(170.00, 16.00));
-    }
-    */
 }
